1c27848f2b38a330b92bee1849de707da0b76d63
refaa <narefa395@gmail.com>
2025-05-04 11:08:44 +0700

    mengubah file index php

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
-echo"hello word";
+
+
+echo "hello word";
+?>
+
+<?php
+
+$name = "refa";
+$major = "Infor";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Awal</title>
+</head>
+<body>
+    <h1> Halo nama saya <?php echo $name ?> </h1>
+    <h1> Saya dari jurusan <?php echo $name ?> </h1>
+</body>
+</html>
